75422dbf49520bf13b8a769ae29c2e67b649ed22
Badal kumar <s.k.badal@outlook.com>
2018-01-11 00:33:48 +0530

    Release 3.0 Lunch Screen

--- app/src/main/java/com/example/my_lenovo/clanguagetipsandtricks/Definations.java
+++ app/src/main/java/com/example/my_lenovo/clanguagetipsandtricks/Definations.java
@@ -2,6 +2,10 @@ package com.example.my_lenovo.clanguagetipsandtricks;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
+import android.text.Spanned;
+import android.webkit.WebView;
+import android.widget.TextView;
 
 public class Definations extends AppCompatActivity {
 
@@ -9,5 +13,15 @@ public class Definations extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_definations);
+//        WebView webView = new WebView(this);
+
+//        setContentView(webView);
+        String str = getString(R.string.html);
+        Spanned spn = Html.fromHtml(str);
+        TextView textView = findViewById(R.id.definations);
+        textView.setText(spn);
+//        webView.loadData(str,"text/html",null);
+//        webView.loadDataWithBaseURL(null, str,"text/html","utf-8",null);
+
     }
 }
