16c3ae870222e783840ef456eeb9cacad37976b7
Badal kumar <s.k.badal@outlook.com>
2018-01-11 16:03:45 +0530

    Release 3.0 Content Datatypes

--- app/src/main/java/com/example/my_lenovo/clanguagetipsandtricks/Definations.java
+++ app/src/main/java/com/example/my_lenovo/clanguagetipsandtricks/Definations.java
@@ -13,15 +13,6 @@ public class Definations extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_definations);
-//        WebView webView = new WebView(this);
-
-//        setContentView(webView);
-        String str = getString(R.string.html);
-        Spanned spn = Html.fromHtml(str);
-        TextView textView = findViewById(R.id.definations);
-        textView.setText(spn);
-//        webView.loadData(str,"text/html",null);
-//        webView.loadDataWithBaseURL(null, str,"text/html","utf-8",null);
 
     }
 }
